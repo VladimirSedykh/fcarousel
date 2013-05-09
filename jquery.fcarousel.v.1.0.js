7e0fa14011256556b37ab30ed921621cf92de5bf
Vladimir Sedykh <SWA88@mail.ru>
2013-05-09 13:41:46 +0300

    Version 1.1

--- jquery.fcarousel.v.1.0.js
+++ jquery.fcarousel.v.1.0.js
@@ -1,6 +1,6 @@
 /*
 
- Мой тестовый плагин для создания карусели v 1.0.
+ Мой тестовый плагин для создания карусели v 1.1.
 
  ---------------------------------
 
@@ -11,7 +11,7 @@
  showControls:  true
  stepTime :     500,
  delay :        5000,
- elWidth :      нет дефолтного значения
+ elWidth :      нет дефолтного значения (устанавливает длину внутренних эелментов карусели)
 
  ----------------------------------
 
@@ -41,7 +41,7 @@
 (function($){
    $.fn.myCarousel = function(options) {
 
-      // Стандартные опции (доп опция: elWidth : ...)
+      // Стандартные опции (доп опция: elWidth : ...).
       var settings = {
          visible: 3,
          autoPlay: false,
@@ -52,40 +52,41 @@
          delay : 5000
       };
 
-      // Пакую стили
-      var css = {
-         arrow : {
-            all : {
-               display : (settings.showControls === false) ? 'none' : 'block',
-               width : '50',
-               height : '50',
-               background: 'green',
-               cursor : 'pointer',
-               position : 'absolute',
-               bottom : 0
-            },
-            right : {
-               background : 'red',
-               right : 0
-            }
-         },
-         ul : {
-            position : 'relative',
-            width : 9999 + 'px',
-            left : 0
-         }
-      }
-
       return this.each(function() {
 
+         // Обновляю стандартные опции.
          if (options) {
             $.extend(settings, options);
          }
 
-         // Пакую функции
+         // Пакую стили карусели.
+         var css = {
+            arrow : {
+               all : {
+                  display : (settings.showControls === false) ? 'none' : 'block',
+                  width : '50',
+                  height : '50',
+                  background: 'green',
+                  cursor : 'pointer',
+                  position : 'absolute',
+                  bottom : 0
+               },
+               right : {
+                  background : 'red',
+                  right : 0
+               }
+            },
+            ul : {
+               position : 'relative',
+               width : 9999 + 'px',
+               left : 0
+            }
+         }
+
+         // Пакую функции.
          var functions = {
 
-            // Функция стилей
+            // Функция стилей.
             styles : function () {
                $this.css({
                   'position': 'relative',
@@ -94,7 +95,7 @@
                });
                $ul.css(css.ul);
                $ul.children().each(function() {
-                  // оборачиваю li элементы в боксы
+                  // оборачиваю li элементы в боксы.
                   $(this).wrap('<div class="el-wrap">')
                });
                $('.el-wrap').css({
@@ -115,7 +116,7 @@
                $('#arrow-right').css(css.arrow.right);
             },
 
-            // Функция движения
+            // Функция движения.
             m0ve : function(el) {
                var dir = (el.attr('id') === settings.prev) ? 'prev' : 'next';
                (dir === 'next') ? cur-=1 : cur+=1;
@@ -125,7 +126,7 @@
                }, settings.stepTime);
             },
 
-            // Функция опеределяет самый длинный элемент карусели
+            // Функция опеределяет самый длинный элемент карусели.
             longestChild : function(el) {
                var b = 0;
                var li = el.children();
@@ -138,28 +139,28 @@
                return b;
             },
 
-            // Функция задает автоматическую прокрутку карусели
+            // Функция задает автоматическую прокрутку карусели.
             isAutoplay : function() {
                (settings.autoPlay === true) ? setInterval(function(){ functions.m0ve($this); }, settings.delay) : false ;
             }
 
          }
 
-         // Задаю "глобальные" переменные
+         // Задаю "глобальные" переменные.
          var $this = $(this);
          var $ul = $this.children(':first');
          var itemsTotal = $ul.children().length;
          var longest_child = settings.elWidth || functions.longestChild($ul);  // определяю самый длинный li элемент
-         var cur = 0; // текущий шаг
+         var cur = 0; // текущий шаг.
 
-         // Применяю css стили
+         // Применяю css стили.
          functions.styles();
 
-         // Функция движения при клике на кнопки навигации
+         // Функция движения при клике на кнопки навигации.
          $('.nav-arrow').click(function(){
             functions.m0ve($(this));
          });
-         // Если выбрана опция autoplay
+         // Если выбрана опция autoplay.
          functions.isAutoplay();
 
       });
--- main.js
+++ main.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
    $('.test-carousel').myCarousel({
       visible : 3,
       stepTime : 1000,
-      autoPlay : false
+      autoPlay : true,
+      showControls: false
    });
 
 });
